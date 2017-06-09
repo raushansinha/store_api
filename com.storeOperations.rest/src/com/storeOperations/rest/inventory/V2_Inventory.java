2ca7ce2b683ca0d456bc0124b15dabad5d0c1502
Raushan Sinha <hr.raushan@gmail.com>
2017-06-09 16:48:25 -0400

    Comppeted POST

--- com.storeOperations.rest/src/com/storeOperations/rest/inventory/V2_Inventory.java
+++ com.storeOperations.rest/src/com/storeOperations/rest/inventory/V2_Inventory.java
@@ -1,12 +1,15 @@
 package com.storeOperations.rest.inventory;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.*;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jettison.json.JSONArray;
 
 import java.sql.*;
@@ -20,7 +23,7 @@ public class V2_Inventory {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getAnItem(
-			@QueryParam("code") String code) 
+			@QueryParam("brand") String brand) 
 			throws Exception {
 		
 		PreparedStatement query = null;
@@ -29,7 +32,7 @@ public class V2_Inventory {
 		
 		try {
 			
-			if(code == null) {
+			if(brand == null) {
 				return Response.status(400).entity("Error: Please specify item code for this serach").build();
 			}
 			
@@ -37,7 +40,7 @@ public class V2_Inventory {
 			
 			JSONArray json = new JSONArray();
 			
-			json = dao.queryReturnAnItem(code);
+			json = dao.queryReturnItemForBrand(brand);
 			
 			rb = Response.ok(json.toString()).build();
 			
@@ -60,28 +63,23 @@ public class V2_Inventory {
 	}
 */
 	
-	@Path("/{code}")
+	@Path("/{brand}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getItem(
-			@PathParam("code")String code) 
+			@PathParam("brand")String brand) 
 			throws Exception {
 		
 		Connection conn = null;
 		Response rb = null;
 		
 		try {
-			System.out.println("Code: " + code);
-			
-			if(code == null) {
-				return Response.status(400).entity("Error: please specify item code for this serach").build();
-			}
 			
 			StoreSchema dao  = new StoreSchema();
 			
 			JSONArray json = new JSONArray();
 			
-			json = dao.queryReturnAnItem(code);
+			json = dao.queryReturnItemForBrand(brand);
 			
 			rb = Response.ok(json.toString()).build();
 			
@@ -94,5 +92,114 @@ public class V2_Inventory {
 		
 		return rb;
 	}
+	
+	/*@Path("/{stock}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getStock(
+			@PathParam("stock") int stock) 
+			throws Exception {
+		
+		Connection conn = null;
+		Response rb = null;
+		
+		try {
+						
+			StoreSchema dao  = new StoreSchema();
+			
+			JSONArray json = new JSONArray();
+			
+			json = dao.queryReturnStock(stock);
+			
+			rb = Response.ok(json.toString()).build();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(conn != null) conn.close();
+		}
+		
+		return rb;
+	}*/
+	
+	@Path("/{brand}/{code}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getItemStock(
+			@PathParam("brand") String brand,
+			@PathParam("code") String code) 
+			throws Exception {
+		
+		Connection conn = null;
+		Response rb = null;
+		
+		try {
+						
+			StoreSchema dao  = new StoreSchema();
+			
+			JSONArray json = new JSONArray();
+			
+			json = dao.queryReturnItemForBrandAndCode(brand, code);
+			
+			rb = Response.ok(json.toString()).build();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(conn != null) conn.close();
+		}
+		
+		return rb;
+	}
+	
+	@POST
+	@Path("/create")
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED, MediaType.APPLICATION_JSON})
+	//@Consumes(MediaType.APPLICATION_FORM_URLENCODED) // Only web form
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addStoreItem(String incomingData) throws Exception {
+		String returnString = null;
+		JSONArray jsonArray = new JSONArray();
+		StoreSchema dao = new StoreSchema();
+		
+		try {
+			
+			System.out.println("incoming Data: " + incomingData);
+			
+			ObjectMapper mapper = new ObjectMapper();
+			ItemEntry itemEntry = mapper.readValue(incomingData, ItemEntry.class);
+			
+			int http_code = dao.insertIntoItem_Master(itemEntry.BRAND, 
+													  itemEntry.CODE, 
+													  itemEntry.DESCRIPTION, 
+													  itemEntry.UNIT_OF_MEASURE, 
+													  itemEntry.UNIT_PRICE_INR, 
+													  itemEntry.AVAILABLE_STOCK);
+			
+			if(http_code == 200) {
+				//returnString = jsonArray.toString();
+				returnString = "Item Inserted";
+			} else {
+				return Response.status(500).entity("Unable to process Item").build();
+			}
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		} 
+		
+		return Response.ok(returnString).build();
+	}
+}
 
+class ItemEntry {
+	public String BRAND;
+	public String CODE;
+	public String DESCRIPTION;
+	public String UNIT_OF_MEASURE;
+	public String UNIT_PRICE_INR;
+	public String AVAILABLE_STOCK;
 }
